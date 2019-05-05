b84aa77e32178d44d79a4edf55aea2d1d9341aea
guidezpl <guidezpl@users.noreply.github.com>
2019-05-04 22:36:51 -0400

    create initial package

---
+++ spec/git-scroll-marker-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GitScrollMarkerView from '../lib/git-scroll-marker-view';
+
+describe('GitScrollMarkerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
